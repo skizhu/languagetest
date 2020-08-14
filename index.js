ce3263818e5203cf569724f7866e0431864ca7d1
skizhu <69230144+skizhu@users.noreply.github.com>
2020-08-14 16:53:31 -0400

    Added Chinese, German

--- index.js
+++ index.js
@@ -19,11 +19,29 @@ var langQuestions = {
 	},
 
 	question3:{
-		question:"који је ово језик",
+		question:"који је ово језик?",
 		options:{
 		option1:"Serbian",
 		option2:"Russian",
 		option3:"Polish"
+		}
+	},
+	
+	question4:{
+		question:"这是哪种语言?",
+		options:{
+		option1:"Japanese",
+		option2:"Sinhalese",
+		option3:"Chinese"
+		}
+	},
+	
+	question5:{
+		question:"Welche Sprache ist das?",
+		options:{
+		option1:"German",
+		option2:"Croatian",
+		option3:"Dutch"
 		}
 	}
 
@@ -35,16 +53,16 @@ var langQuestions = {
 
 
 function questEk(){
+	document.getElementById("score").innerHTML = "0/10";
+	
 	document.getElementById("langText").innerHTML = langQuestions.question1.question;
 	document.getElementById("optionOne").innerHTML = langQuestions.question1.options.option1;
 	document.getElementById("optionTwo").innerHTML = langQuestions.question1.options.option2;
 	document.getElementById("optionThree").innerHTML = langQuestions.question1.options.option3;
 	document.getElementById("optionTwo").addEventListener("click", questUma);
-}
+};
 
 function questUma(){
-	//var correctAnswer = document.getElementById("optionTwo");
-	//correctAnswer.id = "correctAnswer";
 	document.getElementById("score").innerHTML = "1/10";
 
 	document.getElementById("langText").innerHTML = langQuestions.question2.question;
@@ -52,17 +70,35 @@ function questUma(){
 	document.getElementById("optionTwo").innerHTML = langQuestions.question2.options.option2;
 	document.getElementById("optionThree").innerHTML = langQuestions.question2.options.option3;
 	document.getElementById("optionThree").addEventListener("click", questJedan);
-}
+};
 
 function questJedan(){
-	//var correctAnswer = document.getElementById("optionTwo");
-	//correctAnswer.id = "correctAnswer";
 	document.getElementById("score").innerHTML = "2/10";
 
 	document.getElementById("langText").innerHTML = langQuestions.question3.question;
 	document.getElementById("optionOne").innerHTML = langQuestions.question3.options.option1;
 	document.getElementById("optionTwo").innerHTML = langQuestions.question3.options.option2;
 	document.getElementById("optionThree").innerHTML = langQuestions.question3.options.option3;
+	document.getElementById("optionOne").addEventListener("click", questYi);
+};
+
+function questYi(){
+	document.getElementById("score").innerHTML = "3/10";
+
+	document.getElementById("langText").innerHTML = langQuestions.question4.question;
+	document.getElementById("optionOne").innerHTML = langQuestions.question4.options.option1;
+	document.getElementById("optionTwo").innerHTML = langQuestions.question4.options.option2;
+	document.getElementById("optionThree").innerHTML = langQuestions.question4.options.option3;
+	document.getElementById("optionOne").addEventListener("click", questEin);
+};
+
+function questEin(){
+	document.getElementById("score").innerHTML = "4/10";
+
+	document.getElementById("langText").innerHTML = langQuestions.question5.question;
+	document.getElementById("optionOne").innerHTML = langQuestions.question5.options.option1;
+	document.getElementById("optionTwo").innerHTML = langQuestions.question5.options.option2;
+	document.getElementById("optionThree").innerHTML = langQuestions.question5.options.option3;
 	document.getElementById("optionOne").addEventListener("click", questEk);
-}
+};
 
